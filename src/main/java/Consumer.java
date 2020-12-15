bd09bf5961bba0fe5a5bc4a5c6f34c3498802ede
Sergei Panin <sergeipanin1993@gmail.com>
2020-12-15 21:37:29 +0300

    Initial commit

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable {
+    private BlockingQueue queue;
+    public Consumer(BlockingQueue queue){
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            while(true) {
+                System.out.println(queue.take());
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+
+    }
+}
